cfc69554b9eae2e538428a7993288914326ea1a4
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-06-06 16:31:25 +1000

    design: sign up page done

--- client/src/Pages.tsx
+++ client/src/Pages.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Dashboard from './screens/Dashboard';
 import Workout from './screens/Workout';
+import SignUp from './screens/SignUp';
 
 const Pages = () => {
   return (
@@ -9,6 +10,7 @@ const Pages = () => {
       <Routes>
 				<Route path="/" element={<Dashboard />} />
         <Route path="/workout" element={<Workout />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </>
   );

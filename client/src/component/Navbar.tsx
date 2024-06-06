b3b761ea5231306b5fdd5e671dfd5efd43930f46
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-06-06 22:01:34 +1000

    Design: some dashboard images done

--- client/src/component/Navbar.tsx
+++ client/src/component/Navbar.tsx
@@ -18,11 +18,11 @@ import {
 const Navbar = () => {
   const navigate = useNavigate();
 
-  const mobileScreen = useMediaQuery('(max-width: 950px)');
+  const mobileScreen = useMediaQuery('(max-width: 1020px)');
 
   return (
-    <div className="bg-black w-screen h-[80px] justify-center flex z-50 ovcerflow-x-hidden">
-      <div className="flex text-white max-w-[1200px] w-[100%] justify-between items-center mx-[100px]">
+    <div className="fixed top-0 bg-black opacity-80 w-full h-[80px] justify-center flex z-50 overflow-x-hidden">
+      <div className={`flex text-white opacity-100 max-w-[1200px] w-[100%] justify-between items-center ${mobileScreen ? 'mx-[20px]' : 'mx-[100px]'}`}>
         <Button className="text-2xl">BoulderBoost</Button>
         {mobileScreen ?
             <Sheet>
@@ -47,8 +47,12 @@ const Navbar = () => {
               <Button>Sydney Gyms</Button>
               <Button>About Me</Button>
               <div className="h-[25px] border-r-[1px] border-white mx-5"></div>
+
               <section className="flex gap-5">
-                <Button onClick={() => navigate('/signup')} className="border-[1px] rounded-[10px] hover:text-black hover:bg-white">Sign In</Button>
+                <Button onClick={() => navigate('/signup')} className="h-[35px] border-[1px] text-black bg-white hover:text-white hover:bg-black">Sign In</Button>
+              </section>
+              <section className="flex gap-5">
+                <Button onClick={() => navigate('/signup')} className="h-[35px] border-[1px] text-white bg-black hover:border-blue-500">FAQs</Button>
               </section>
             </section>
           </>
--- client/src/screens/Dashboard.tsx
+++ client/src/screens/Dashboard.tsx
@@ -1,6 +1,6 @@
 import RockClimbing from '@/assets/RockClimbing.jpg';
 import Navbar from '@/component/Navbar';
-
+import Blochaus from '@/assets/Blochaus.jpeg';
 const Dashboard = () => {
 
   return (
@@ -8,18 +8,33 @@ const Dashboard = () => {
       <Navbar />
 
       {/* main */}
-      <section className="w-full h-screen bg-black mt-[-80px] relative">
-        <div className="absolute inset-0 bg-cover bg-center mt-[80px]"
+      <section className="w-full h-screen bg-black relative">
+        <div className="absolute inset-0 bg-cover bg-center mt-[80px] z-0"
           style={{
             backgroundImage: `url(${RockClimbing})`,
-            // opacity: 0.7
+            opacity: 0.8
           }}
         />
+
+        <div className="h-full mx-[100px] flex flex-col justify-center">
+          <section className="max-w-[1200px] mx-auto flex text-white text-[50px] opacity-90">
+            Keep pushing past your limits
+          </section>
+        </div>
         
       </section>
 
-      <section className="w-screen h-screen">
-            s
+      <section className="w-full h-screen bg-black">
+        My name is
+      </section>
+
+      <section className="w-full h-screen bg-black">
+        <div className="w-full h-full bg-cover bg-center  z-0"
+          style={{
+            backgroundImage: `url(${Blochaus})`,
+            opacity: 0.8
+          }}
+        />
       </section>
     </div>
   )

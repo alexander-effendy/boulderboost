bd53dd9d053ac092b556628e2eef2b11f94ed5da
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-06-06 15:53:26 +1000

    setup: vite react ts + tailwindcss

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <section className="bg-blue-200 text-3xl grid place-items-center h-screen w-screen">
+      Welcome to my world
+    </section>
+  )
+}
+
+export default App
